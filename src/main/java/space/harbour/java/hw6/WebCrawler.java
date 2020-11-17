9d22405beffd1bf51c2c604e6502546e9f97b5c6
patriciaporal <patriciaann.poral@gmail.com>
2020-11-17 15:48:46 +0800

    HW6

--- src/main/java/space/harbour/java/hw6/WebCrawler.java
+++ src/main/java/space/harbour/java/hw6/WebCrawler.java
@@ -22,18 +22,17 @@ public class WebCrawler {
     public static class UrlVisitor implements Runnable {
         public static String getContentOfWebPage(URL url) {
             final StringBuilder content = new StringBuilder();
-        try (InputStream is = url.openConnection().getInputStream();
-             InputStreamReader in = new InputStreamReader(is, StandardCharsets.UTF_8);
-             BufferedReader br = new BufferedReader(in)) {
-            String inputLine;
-            while (( inputLine = br.readLine() ) != null)
-                content.append(inputLine);
-        } catch (IOException e) {
-            System.out.println("Failed to retrieve content of " + url.toString());
-            e.printStackTrace();
-        }
+            try (InputStream is = url.openConnection().getInputStream();
+                 InputStreamReader in = new InputStreamReader(is, StandardCharsets.UTF_8);
+                 BufferedReader br = new BufferedReader(in)) {
+                String inputLine;
+                while (( inputLine = br.readLine() ) != null) content.append(inputLine);
+            } catch (IOException e) {
+                System.out.println("Failed to retrieve content of " + url.toString());
+                e.printStackTrace();
+            }
 
-        return content.toString();
+            return content.toString();
         }
 
         @Override
