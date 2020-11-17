964220d5ddbd1ec89ea3b8fa208cb31f36bfccd2
patriciaporal <patriciaann.poral@gmail.com>
2020-11-17 21:15:35 +0800

    HW6

--- src/test/java/space/harbour/java/hw6/WebCrawlerTest.java
+++ src/test/java/space/harbour/java/hw6/WebCrawlerTest.java
@@ -6,79 +6,75 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+
+import org.junit.Assert;
 import org.junit.Test;
 
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static space.harbour.java.hw6.WebCrawler.alreadyVisited;
-import static space.harbour.java.hw6.WebCrawler.toVisit;
 
 public class WebCrawlerTest {
     @Test
     public void test1() throws MalformedURLException {
-        toVisit.add(new URL("https://vasart.github.io/supreme-potato/"));
+        WebCrawler.toVisit.add(new URL("https://vasart.github.io/supreme-potato/"));
         ExecutorService executorService = Executors.newFixedThreadPool(10);
-        while (!toVisit.isEmpty()) {
+        while (!WebCrawler.toVisit.isEmpty()) {
             executorService.submit(new WebCrawler.UrlVisitor());
         }
         try {
             executorService.awaitTermination(5, TimeUnit.SECONDS);
             if (executorService.isTerminated()) {
-                System.err.println("Timed out waiting for executor to terminate cleanly. Shutting down.");
+                System.err.println("Timed out. Shutting down.");
                 executorService.shutdownNow();
             }
         } catch (InterruptedException e) {
             System.err.println("Interrupted while waiting for executor shutdown.");
             Thread.currentThread().interrupt();
         }
-        System.out.println(alreadyVisited);
-        System.out.println(toVisit);
-        assertEquals(4, alreadyVisited.size());
+        System.out.println(WebCrawler.alreadyVisited);
+        System.out.println(WebCrawler.toVisit);
+        Assert.assertEquals(4, WebCrawler.alreadyVisited.size());
     }
 
     @Test
     public void test2() throws MalformedURLException {
-        toVisit.add(new URL("https://vasart.github.io/supreme-potato/"));
+        WebCrawler.toVisit.add(new URL("https://vasart.github.io/supreme-potato/"));
         ExecutorService executorService = Executors.newFixedThreadPool(10);
-        while (!toVisit.isEmpty()) {
+        while (!WebCrawler.toVisit.isEmpty()) {
             executorService.submit(new WebCrawler.UrlVisitor());
         }
         try {
             executorService.awaitTermination(5, TimeUnit.SECONDS);
             if (executorService.isTerminated()) {
-                System.err.println("Timed out waiting for executor to terminate cleanly. Shutting down.");
+                System.err.println("Timed out. Shutting down.");
                 executorService.shutdownNow();
             }
         } catch (InterruptedException e) {
             System.err.println("Interrupted while waiting for executor shutdown.");
             Thread.currentThread().interrupt();
         }
-        System.out.println(alreadyVisited);
-        System.out.println(toVisit);
-        assertTrue(null, toVisit.isEmpty());
+        System.out.println(WebCrawler.alreadyVisited);
+        System.out.println(WebCrawler.toVisit);
+        Assert.assertTrue(null, WebCrawler.toVisit.isEmpty());
     }
 
     @Test
     public void test3() throws MalformedURLException {
-        toVisit.add(new URL("https://vasart.github.io/supreme-potato/"));
+        WebCrawler.toVisit.add(new URL("https://vasart.github.io/supreme-potato/"));
         ExecutorService executorService = Executors.newFixedThreadPool(10);
-        while (!toVisit.isEmpty()) {
+        while (!WebCrawler.toVisit.isEmpty()) {
             executorService.submit(new WebCrawler.UrlVisitor());
         }
         try {
             executorService.awaitTermination(5, TimeUnit.SECONDS);
             if (executorService.isTerminated()) {
-                System.err.println("Timed out waiting for executor to terminate cleanly. Shutting dow   n.");
+                System.err.println("Timed out. Shutting dow   n.");
                 executorService.shutdownNow();
             }
         } catch (InterruptedException e) {
             System.err.println("Interrupted while waiting for executor shutdown.");
             Thread.currentThread().interrupt();
         }
-        System.out.println(alreadyVisited);
-        System.out.println(toVisit);
-        assertFalse(alreadyVisited.isEmpty());
+        System.out.println(WebCrawler.alreadyVisited);
+        System.out.println(WebCrawler.toVisit);
+        Assert.assertFalse(WebCrawler.alreadyVisited.isEmpty());
     }
 }
